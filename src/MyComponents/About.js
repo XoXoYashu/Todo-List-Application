5a8c12ff7c1c81faae5c3d672b18fbaf1be2761e
XoXoYashu <54573388+XoXoYashu@users.noreply.github.com>
2022-07-04 15:18:02 +0530

    Add files via upload
    This is a Todo List application made using React.js

---
+++ src/MyComponents/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const About = () => {
+  return (
+    <div>
+        This is About
+        <p>duvhiovhukwahvilhdavhdljvckldsvkldkcljdwklvjckldvdvdjvl
+            sdvjdlvjdljvlkdjlivjlavjdlsjvlkvjd;ovavkl;
+            4d;vljds;vjdlhvkdsjklvhaskucs,adcmn kdlcjcdv
+        </p>
+    </div>
+  )
+}
